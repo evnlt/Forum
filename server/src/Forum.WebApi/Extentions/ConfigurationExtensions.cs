c3a4714cb2a54853d38733f625994108d01aa705
Ivan Lutsenko <evanlt187@gmail.com>
2024-02-05 18:02:06 +0200

    Added posts api endpoint

---
+++ server/src/Forum.WebApi/Extentions/ConfigurationExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Forum.WebApi.Extentions;
+
+public static class ConfigurationExtensions
+{
+    private const string DbConnectionStringKey = "ForumDB";
+
+    public static string GetDbConnectionString(this IConfiguration configuration)
+    {
+        return configuration.GetConnectionString(DbConnectionStringKey) ?? throw new KeyNotFoundException();
+    }
+}
--- server/src/Forum.WebApi/Program.cs
+++ server/src/Forum.WebApi/Program.cs
@@ -1,6 +1,16 @@
+using Forum.WebApi;
+using Forum.WebApi.Extentions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetDbConnectionString()));
+    
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("api/posts",
+    async (ApplicationDbContext applicationDbContext) => await applicationDbContext.Posts.AsNoTracking().ToListAsync());
 
 app.Run();
