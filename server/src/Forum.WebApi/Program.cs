64a675d46f9919359b937ee63381e3292dee197e
Ivan Lutsenko <evanlt187@gmail.com>
2024-02-06 17:15:22 +0200

    Added client cors

--- server/src/Forum.WebApi/Program.cs
+++ server/src/Forum.WebApi/Program.cs
@@ -1,16 +1,29 @@
 using Forum.WebApi;
 using Forum.WebApi.Extentions;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetDbConnectionString()));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("client",
+        policy =>
+        {
+            // TODO - fix env variables
+            policy.WithOrigins("http://localhost:5173")
+                .AllowCredentials()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
     
 var app = builder.Build();
 
-app.MapGet("api/posts",
-    async (ApplicationDbContext applicationDbContext) => await applicationDbContext.Posts.AsNoTracking().ToListAsync());
+app.UseCors("client");
+
+app.MapGet("api/posts", async (ApplicationDbContext applicationDbContext) => await applicationDbContext.Posts.AsNoTracking().ToListAsync());
 
 app.Run();
