c11a955b3acd0300a2ede9e46ce7240102a8e68f
Ivan Lutsenko <evanlt187@gmail.com>
2024-02-07 11:28:10 +0200

    Added react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,13 @@
 import { PostsList } from "./components/PostsList";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
-  return <PostsList />;
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<PostsList />} />
+        <Route path="/posts/:id" element={null} />
+      </Routes>
+    </div>
+  );
 }
--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { getPosts } from "../services/posts";
+import { Link } from "react-router-dom";
 
 export function PostsList() {
   const [posts, setPosts] = useState([]);
@@ -11,7 +12,7 @@ export function PostsList() {
   return posts.map((post) => {
     return (
       <h1 key={post.id}>
-        <a>{post.title}</a>
+        <Link to={`/posts/${post.id}`}>{post.title}</Link>
       </h1>
     );
   });
