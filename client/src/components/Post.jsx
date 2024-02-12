401039e5f6a2d493ad72ae2ec15a33f6713e56c1
Ivan Lutsenko <evanlt187@gmail.com>
2024-02-12 13:03:02 +0200

    Added displaying root comments

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,11 +1,21 @@
 import { usePost } from "../contexts/PostContext";
+import { CommentList } from "./CommentList";
 
 export function Post() {
-  const { post } = usePost();
+  const { post, rootComments } = usePost();
 
   return (
     <>
       <h1>{post.title}</h1>
+      <article>{post.body}</article>
+      <h3>Comments</h3>
+      <section>
+        {rootComments != null && rootComments.length > 0 && (
+          <div>
+            <CommentList comments={rootComments} />
+          </div>
+        )}
+      </section>
     </>
   );
 }
--- client/src/contexts/PostContext.jsx
+++ client/src/contexts/PostContext.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import { useAsync } from "../hooks/useAsync";
 import { getPost } from "../services/posts";
 import { useParams } from "react-router-dom";
@@ -12,8 +12,28 @@ export function usePost() {
 export function PostProvider({ children }) {
   const { id } = useParams();
   const { loading, error, value: post } = useAsync(() => getPost(id), [id]);
+  const commentsByParentId = useMemo(() => {
+    if (post?.comments == null) return [];
+    const group = {};
+    post.comments.forEach((comment) => {
+      group[comment.parentId] ||= [];
+      group[comment.parentId].push(comment);
+    });
+    return group;
+  }, [post?.comments]);
+
+  function getReplies(parentId) {
+    return commentsByParentId[parentId];
+  }
+
   return (
-    <Context.Provider value={{ post: { id, ...post } }}>
+    <Context.Provider
+      value={{
+        post: { id, ...post },
+        rootComments: commentsByParentId[null],
+        getReplies,
+      }}
+    >
       {loading ? <h1>Loading</h1> : error ? <h1>{error}</h1> : children}
     </Context.Provider>
   );
