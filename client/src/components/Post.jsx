5f9a8426d8b395d533dd64f1bcb0451ccc11d520
Ivan Lutsenko <evanlt187@gmail.com>
2024-02-13 16:25:44 +0200

    Added create comment action

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,9 +1,22 @@
 import { usePost } from "../contexts/PostContext";
+import { useAsyncFn } from "../hooks/useAsync";
+import { createComment } from "../services/comments";
 import { CommentForm } from "./CommentForm";
 import { CommentList } from "./CommentList";
 
 export function Post() {
   const { post, rootComments } = usePost();
+  const {
+    loading,
+    error,
+    execute: createCommentFn,
+  } = useAsyncFn(createComment);
+
+  function onCommentCreate(message) {
+    return createCommentFn({ postId: post.id, message }).then((comment) => {
+      console.log(comment);
+    });
+  }
 
   return (
     <>
@@ -11,7 +24,11 @@ export function Post() {
       <article>{post.body}</article>
       <h3>Comments</h3>
       <section>
-        <CommentForm />
+        <CommentForm
+          loading={loading}
+          error={error}
+          onSubmit={onCommentCreate}
+        />
         {rootComments != null && rootComments.length > 0 && (
           <div>
             <CommentList comments={rootComments} />
