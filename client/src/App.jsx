57568af248e22a057b0fdd87217dd9ad7fdf6bfe
Ivan Lutsenko <evanlt187@gmail.com>
2024-03-01 12:35:56 +0200

    Added navigating to main page after login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,27 @@
 import { PostsList } from "./components/PostsList";
 import { Routes, Route } from "react-router-dom";
 import { PostProvider } from "./contexts/PostContext";
+import { IdentityProvider } from "./contexts/IdentityContext";
 import { Post } from "./components/Post";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
   return (
     <div className="container">
-      <Routes>
-        <Route path="/" element={<PostsList />} />
-        <Route
-          path="/posts/:id"
-          element={
-            <PostProvider>
-              <Post />
-            </PostProvider>
-          }
-        />
-      </Routes>
+      <IdentityProvider>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<PostsList />} />
+          <Route
+            path="/posts/:id"
+            element={
+              <PostProvider>
+                <Post />
+              </PostProvider>
+            }
+          />
+        </Routes>
+      </IdentityProvider>
     </div>
   );
 }
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,30 +2,23 @@ import { useState } from "react";
 import { useAsyncFn } from "../hooks/useAsync";
 import { login } from "../services/identity";
 import { useIdentity } from "../contexts/IdentityContext";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { loading, error, execute: loginFn } = useAsyncFn(login);
-
   const { setAccessToken } = useIdentity();
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     loginFn({ email, password }).then((accessToken) => {
       setAccessToken(accessToken);
+      navigate("/");
     });
-    // try {
-    //   // Call your backend API to authenticate user
-    //   const response = await axios.post("/api/auth/login", { email, password });
-    //   const { token, user } = response.data;
-    //   localStorage.setItem("token", token);
-    //   setUser(user);
-    // } catch (error) {
-    //   console.error("Login error:", error);
-    //   throw error;
-    // }
   };
 
   return (
